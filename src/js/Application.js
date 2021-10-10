0e6a86d0a869d8ba2965f6c830401027f4865437
Donyo Donev <donevdonyo39@gmail.com>
2021-10-10 13:03:41 +0300

    animejs animation added on article element click

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,16 +1,28 @@
 import EventEmitter from "eventemitter3";
 import anime from "animejs";
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
-    this.init();
-    this.emit(Application.events.READY);
-  }
-  init() {}
+    constructor() {
+        super();
+        this.init();
+        this.emit(Application.events.READY);
+    }
+    init() {
+        const element = document.querySelector('.article');
+        element.addEventListener('click', () => {
+            console.log('element clicked');
+            anime({
+                targets: '.article',
+                translateX: 250,
+                direction: 'alternate',
+                loop: true,
+                easing: 'spring(1, 80, 10, 0)'
+            })
+        });
+    }
 }
